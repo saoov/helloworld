b03ee7b3e97ec321583da2e5701bf6654d0753f6
SeongA Jo <seongajo@SeongAui-MacBookAir.local>
2021-08-01 23:24:17 +0900

    first commit

---
+++ src/main/java/com/example/saoov/SaoovApplication.java
@@ -0,0 +1,13 @@
+package com.example.saoov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaoovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaoovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/saoov/SaoovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saoov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaoovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
