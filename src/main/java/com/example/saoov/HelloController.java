10c022c76e8ffb83efa3394dea055f4e2739a6c7
SeongA Jo <seongajo@SeongAui-MacBookAir.local>
2021-08-02 23:34:50 +0900

    test

--- src/main/java/com/example/saoov/HelloController.java
+++ src/main/java/com/example/saoov/HelloController.java
@@ -11,6 +11,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello, SAOOV World!!!!!";
+        return "Hello, SAOOV World!!!!!!!!!";
     }
 }
